3d8645dab0fc9afba7dcc7d5e5df41db80349b6c
Harsh Kumar <221220025@nitdelhi.ac.in>
2024-06-14 16:47:12 +0530

    Up To Date

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const signInBtnLink = document.querySelector('.signInBtn-link');
+  const signUpBtnLink = document.querySelector('.signUpBtn-link');
+  const wrapper = document.querySelector('.wrapper');
+
+  signUpBtnLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    wrapper.classList.add('active');
+  });
+
+  signInBtnLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    wrapper.classList.remove('active');
+  });
+});
